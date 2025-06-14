e1ba6ef094eaf93c7e298eb83ee6079e31e72d82
Pmethnuwani <pmethnuwani@gmail.com>
2025-06-14 15:39:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'v0IlyboL#memeFlDQxTOj5K0X2kGi2gPTGJtqO143GW_dkK8QsD0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'jlpFEI6L#v8Nlqv6j4Blt-oKSnq7RZObSiDZBZf77fw6gXeFs4Zs' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
